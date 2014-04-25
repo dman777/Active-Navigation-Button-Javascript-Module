be16ac899665ec50ba7bfe3bd1e8a4cd5edc671a
dari6499 <darin.hensley@rackspace.com>
2014-04-25 19:57:21 +0000

    Ver. 2.0

--- active_navigation_button.js
+++ active_navigation_button.js
@@ -1,20 +1,29 @@
 (function () {
-    //Enter your slugs here
-    var urlSlug = [ 
-        "create",
-        "completed",
-        "myrequests" ];
-    //Enter the default URL here.
-    //ex.: url(r'^$', OpenRequestList.as_view())
-    var homeSlug = "openrequests"
+    //##############Settings###############
+    //Add your Slugs Here. 
+    //Example: http://10.1.2.33:8000/threads/slugName1/?search=-latest_post_date
+    var urlSlug = [
+        "slugName1",
+        "slugName2",
+        "slugName3",
+        "slugName4"
+ ];
+    //Default Home Tab
+    HOME = "slugName1";
+    //Set what position you want the slug to be extracted from
+    //Position count is backwards from right(end) to left(start). 
+    SLUG_POSITION = 2
+    //##############End Of Settings###############
 
-    var slugName = window.location.pathname.split('/')[1];
+    var slugName = window.location.pathname.split('/');
+    var slugName = slugName[slugName.length - SLUG_POSITION];
     var elementActive;
     urlSlug.some(function (slugList) {
         if (slugName == slugList) {
+            console.log("yes");
             elementActive = slugList;
             return true; }});
-    elementActive = typeof elementActive !== "undefined" ? elementActive: homeSlug;
+    elementActive = typeof elementActive !== "undefined" ? elementActive: HOME;
     var navElement = document.getElementById(elementActive);
     navElement.classList.add("active");
 })();
